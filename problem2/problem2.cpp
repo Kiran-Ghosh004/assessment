ab6b076a9bc6755cbef82f7798e827bbe2537152
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-12-11 00:06:15 +0530

    problem3 done

--- problem2/problem2.cpp
+++ problem2/problem2.cpp
@@ -10,18 +10,14 @@ using namespace std;
 string frequencySort(string s) {
     unordered_map<char,int> freq;
     for (char c : s) freq[c]++;
-
     vector<pair<int,char>> arr;  
     for (auto &p : freq) arr.push_back({p.second, p.first});
-
     sort(arr.begin(), arr.end());      
     reverse(arr.begin(), arr.end());  
-
     string result = "";
     for (auto &p : arr) {
         result.append(p.first, p.second);
     }
-
     return result;
 }
 
---
+++ problem3/problem3.cpp
@@ -0,0 +1,35 @@
+// two sum problem implementation in C++
+// time complexity: O(n)
+// space complexity: O(n)
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>twosum(vector<int>nums,int target){
+    unordered_map<int,int>mp;
+    vector<int>res;
+    for(int i=0;i<nums.size();i++){
+        int complement=target-nums[i];
+        if(mp.find(complement)!=mp.end()){
+            res.push_back(mp[complement]);
+            res.push_back(i);
+            return res;
+        }
+        mp[nums[i]]=i;
+    }
+    return res;
+}
+
+int main(){
+    vector<int>nums={2,7,11,15};
+    int target=9;
+    vector<int>result=twosum(nums,target);
+    for(int i=0;i<result.size();i++){
+        cout<<result[i]<<" ";
+    }
+    return 0;
+}
+
+   
